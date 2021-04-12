381a72a9a229d77f97ae4f04c3fc687d73ebe506
AlexandraPavel <72041403+AlexandraPavel@users.noreply.github.com>
2021-04-12 20:12:49 +0300

    Just a little bit

--- AzureApp/AzureApp/Startup.cs
+++ AzureApp/AzureApp/Startup.cs
@@ -30,9 +30,10 @@ namespace AzureApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-
+            services.AddRazorPages();
             services.AddDbContext<ChallengeDatabaseContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("ChallengeDatabaseContext")));
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,10 +58,10 @@ namespace AzureApp
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
+               /* endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-               
+                    pattern: "{controller=Home}/{action=Index}/{id?}");*/
+                endpoints.MapRazorPages();
             });
         }
     }
